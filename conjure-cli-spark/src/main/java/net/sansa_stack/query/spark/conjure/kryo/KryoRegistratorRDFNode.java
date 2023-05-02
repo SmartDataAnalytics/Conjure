f785efa95882ccfa051b7f04392e309977a1fcb6
Claus Stadler <RavenArkadon@googlemail.com>
2023-05-02 07:50:21 +0200

    Upgrade towards jena 4.8.0

--- conjure-cli-spark/src/main/java/net/sansa_stack/query/spark/conjure/kryo/KryoRegistratorRDFNode.java
+++ conjure-cli-spark/src/main/java/net/sansa_stack/query/spark/conjure/kryo/KryoRegistratorRDFNode.java
@@ -13,23 +13,23 @@ import com.google.gson.Gson;
 
 // TODO Merge into JenaKryoRegistrator in sansa-rdf
 public class KryoRegistratorRDFNode
-	implements KryoRegistrator
+    implements KryoRegistrator
 {
 
-	@Override
-	public void registerClasses(Kryo kryo) {
-		Gson gson = new Gson();
-		//kryo.register(org.apache.jena.rdf.model.RDFNode.class, new RDFNodeSerializer<>(Function.identity(), gson));
-		//kryo.register(org.apache.jena.rdf.model.Resource.class, new RDFNodeSerializer<>(RDFNode::asResource, gson));
-		//kryo.register(org.apache.jena.rdf.model.impl.R.class, new RDFNodeSerializer<>(RDFNode::asResource, gson));
-		kryo.register(ResourceImpl.class, new SerializerRDFNode<>(RDFNode::asResource, gson));
-		kryo.register(ModelCom.class, new SerializerModel(RDFFormat.TURTLE_BLOCKS));
+    @Override
+    public void registerClasses(Kryo kryo) {
+        Gson gson = new Gson();
+        //kryo.register(org.apache.jena.rdf.model.RDFNode.class, new RDFNodeSerializer<>(Function.identity(), gson));
+        //kryo.register(org.apache.jena.rdf.model.Resource.class, new RDFNodeSerializer<>(RDFNode::asResource, gson));
+        //kryo.register(org.apache.jena.rdf.model.impl.R.class, new RDFNodeSerializer<>(RDFNode::asResource, gson));
+        // kryo.register(ResourceImpl.class, new SerializerRDFNode<>(RDFNode::asResource, gson));
+        kryo.register(ModelCom.class, new SerializerModel(RDFFormat.TURTLE_BLOCKS));
 
-		kryo.register(TaskContext.class, new SerializerTaskContext());
+        kryo.register(TaskContext.class, new SerializerTaskContext());
 
-		kryo.register(ConjureConfig.class); //, new SerializerTaskContext());
+        kryo.register(ConjureConfig.class); //, new SerializerTaskContext());
 
-		//kryo.register(DataRefOp.class, new SerializerRDFNode<>(RDFNode::asResource, gson));
-	}
+        //kryo.register(DataRefOp.class, new SerializerRDFNode<>(RDFNode::asResource, gson));
+    }
 
 }
